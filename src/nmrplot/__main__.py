ed6f87ec773b8cd071902913f54b922361d7a1e0
Miguel Arbesú <miguelarbesu@gmail.com>
2021-08-13 15:10:55 +0200

    style(m__main__.py): change plotting defaults
    plot both signs, 42 contour levels

--- src/nmrplot/__main__.py
+++ src/nmrplot/__main__.py
@@ -9,8 +9,8 @@ from nmrplot import core
 @click.argument("path", type=click.Path(exists=True))
 @click.option("-t", "--threshold", type=float)
 @click.option("-c", "--cmap", type=str, default="viridis")
-@click.option("-n", "--nlevs", type=int, default=24)
-@click.option("-s", "--sign", type=str, default="positive")
+@click.option("-n", "--nlevs", type=int, default=42)
+@click.option("-s", "--sign", type=str, default="both")
 def main(path, threshold, cmap, nlevs, sign):
     """Plot a 1D or 2D NMR spectrum"""
     spectrum = core.Spectrum(path)
